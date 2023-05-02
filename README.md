23ea6d6e69be5247d7e7c6afc021704146d8c54b
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2023-05-02 10:10:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# mysite
+
+This package provides an example Django project which is based upon the official tutorial at https://docs.djangoproject.com/en/3.2/intro/tutorial01/.
+
+
+## Why?
+
+Sometimes, I just need a simple Django example project, where the official intro/tutorial project seems to be the perfect fit.
---
+++ polls/admin.py
@@ -0,0 +1,34 @@
+import datetime
+
+from django.contrib import admin
+from django.utils import timezone
+
+from polls.models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+admin.site.register(Question, QuestionAdmin)
